0c7f6046dd0be62311e3ff6cf443d6697256b3c0
tlachman <tristan.lachmann03@gmail.com>
2023-03-06 14:42:46 +0100

    06/03

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,12 @@
 /*   By: event <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 11:24:45 by event             #+#    #+#             */
-/*   Updated: 2023/03/06 13:55:31 by event            ###   ########.fr       */
+/*   Updated: 2023/03/06 14:20:13 by event            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+//#include "libft.h"
+#include <stdlib.h>
 
 int	ft_parts(char *s, char c)
 {
@@ -71,9 +72,9 @@ char	**ft_split(char const *s, char c)
 	char	**res;
 	int		parts;
 
-	parts = ft_parts(s, c);
+	parts = ft_parts((char *)s, c);
 	res = (char **)malloc(sizeof(char *) * (parts + 1));
 	res[parts] = 0;
-	part(res, s, c);
+	part(res, (char *)s, c);
 	return (res);
 }
