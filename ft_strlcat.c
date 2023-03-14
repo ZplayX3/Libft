b9498f10589fa170e53284b9d5f92e287f6af604
tlachman <tristan.lachmann03@gmail.com>
2023-03-14 13:17:10 +0100

    14/03

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: event <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 12:34:16 by event             #+#    #+#             */
+/*   Updated: 2023/03/14 13:05:18 by event            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
@@ -6,6 +18,8 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	size_t	lim;
 	size_t	size_dest;
 
+	if (dest == NULL && size == 0)
+			return (0);
 	size_dest = ft_strlen(dest);
 	lim = size - size_dest - 1;
 	if (size <= size_dest)
